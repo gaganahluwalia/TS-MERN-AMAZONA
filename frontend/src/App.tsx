ab47a97a5bbe12d02dd9f168def50a6dc3cd025e
Gagan Singh <ahluwaliagagan@gmail.com>
2024-03-21 19:03:18 +0000

    Uptill showing image and Product name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   return (
     <div>
       <header>TS Amazona</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map(product => (
+            <li>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved</footer>
     </div>
   )

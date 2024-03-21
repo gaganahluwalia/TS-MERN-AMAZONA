ef27bab064315fdb244b4dc751c88cc80efe4ddb
Gagan Singh <ahluwaliagagan@gmail.com>
2024-03-21 18:39:19 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Typescript MERN AMAZONA
+
+## Lessons
+
+1. Introduction
+2. Install Tools
+3. Create Typescript React App by Vite
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+      <footer>All rights reserved</footer>
+    </div>
+  )
+}
+
+export default App
